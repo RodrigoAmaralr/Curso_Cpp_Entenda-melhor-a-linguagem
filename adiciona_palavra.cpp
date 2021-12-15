4d45ef6433fb1286f031ca87928965db9381928c
Rodrigo Micromed <rodrigo.com2@gmail.com>
2021-12-15 14:33:14 -0300

    Finalizado o modulo 01. Organizando o projeto

---
+++ adiciona_palavra.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "adiciona_palavra.hpp"
+#include "le_arquivo.hpp"
+#include "salvar_arquivo.hpp"
+
+void adiciona_palavra(){
+    std::cout << "Digite a nova palavra, usando letras maiusculas. " << std::endl;
+    std::string nova_palavra;
+    std::cin >> nova_palavra;
+    std::vector<std::string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+    salvar_arquivo(lista_palavras);
+}
